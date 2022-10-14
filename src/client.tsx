4112fd680d52980cdc1d287b6be56504b3fd0fc6
hoonn9 <hun9935@gmail.com>
2022-10-14 17:24:56 +0900

    Update tailwindcss

--- src/client.tsx
+++ src/client.tsx
@@ -1,5 +1,5 @@
 import App from '@layouts/App';
 import React from 'react';
 import { render } from 'react-dom';
-import './styles.css';
+import './main.css';
 render(<App />, document.querySelector('#app'));
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 const scrollbar = require('tailwind-scrollbar');
 
 module.exports = {
-  purge: {
-    enabled: process.env.NODE_ENV === 'production',
-    content: ['./src/**/*.tsx'],
-  },
+  content: ['./src/**/*.{ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
